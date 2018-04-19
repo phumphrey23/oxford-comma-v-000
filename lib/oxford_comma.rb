9fd503a04cf0d991d05a0441943fa5f31917d476
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 00:04:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3
     array[-1].insert(0, "and ")
-    array.join(",")
+    array.join(", ")
   else array.length>3
 
   end
